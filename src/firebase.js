30d56787dc476a33a495c8c748db9a7a55470159
Shivam Kedia <35270677+shivamearth@users.noreply.github.com>
2022-03-02 19:11:45 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyApzCmhqH485_EhkokT35n_OVK4ru9_zJk",
+    apiKey: "asdasdasdasdasdadasd",
     authDomain: "instagram-clone-react-f8d3b.firebaseapp.com",
     databaseURL: "https://instagram-clone-react-f8d3b.firebaseio.com",
     projectId: "instagram-clone-react-f8d3b",
     storageBucket: "instagram-clone-react-f8d3b.appspot.com",
-    messagingSenderId: "465796105891",
-    appId: "1:465796105891:web:2b5bf7251f0b3ac60de116",
-    measurementId: "G-DVQ7E60637"
+    messagingSenderId: "asdasdadasd",
+    appId: "asdasdqweqwe",
+    measurementId: "asfacq"
 });
 
 const db = firebaseApp.firestore();
